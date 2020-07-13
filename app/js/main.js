52ab87f8f01a034edae94cf1a8e0c75c4636d932
shini322 <33940422+shini322@users.noreply.github.com>
2020-07-13 18:04:29 +0300

    Start

---
+++ app/js/main.js
@@ -0,0 +1,30 @@
+var wow = new WOW();
+new WOW().init();
+
+
+$('.instruction__row').slick({
+  infinite: false,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  nextArrow: '<div class="slide-arrow arrow-next"><svg class="icon icon-arrow-right"><use xlink:href="images/sprite.svg#icon-arrow-right"></use></svg></div>',
+  prevArrow: '<div class="slide-arrow arrow-prev"><svg class="icon icon-arrow-right"><use xlink:href="images/sprite.svg#icon-arrow-left"></use></svg></div>'
+});
+
+$(window).scroll(function() {
+  var height = $(window).scrollTop();
+   /*Если сделали скролл на 100px задаём новый класс для header*/
+  if(height > 70){
+  $('header').addClass('header-fixed');
+  } else{
+  /*Если меньше 100px удаляем класс для header*/
+  $('header').removeClass('header-fixed');
+  }
+  });
+
+  $('#wrapper').fullpage({
+    autoScrolling:true,
+    scrollHorizontally: true
+  });
+
+  //methods
+  $.fn.fullpage.setAllowScrolling(false);
